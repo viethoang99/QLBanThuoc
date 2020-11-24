26cf77c78253dc2d7390d997264ced0f2ba80b4d
thanhtung0310 <60694740+thanhtung0310@users.noreply.github.com>
2020-11-24 23:16:40 +0700

    Xóa WCF Service và Sửa thành WService (đã test)

---
+++ QLBanThuoc/WSTest/QLBTService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="QLBTService.asmx.cs" Class="WSTest.QLBanThuocService" %>
---
+++ QLBanThuoc/WSTest/QLBTService.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WSTest
+{
+    /// <summary>
+    /// Summary description for QLBanThuocService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    static class Connection
+    {
+        static string ConnectionString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=QLBanThuoc; Integrated Security=True;Connect Timeout=200";
+        public static SqlConnection conn = new SqlConnection(ConnectionString);
+    }
+    public class QLBanThuocService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string HelloName(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
